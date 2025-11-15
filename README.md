56bb4733ebd99bb420306169452d113580229c85
Priyansh Parekh <parekhpriyansh20@gmail.com>
2025-11-15 16:15:38 +0530

    Add README.md

--- README.md
+++ README.md
@@ -1,1 +1,117 @@
 # RecyclerPlus
+
+---
+**RecyclerPlus** is a lightweight Android library that makes it easy to add modern swipe and drag gestures, icons, and background actions to your RecyclerView items, without all the boilerplate.
+
+## Features
+
+---
+### Left & Right Swipe Actions
+Add smooth swipe gestures on both sides of a RecyclerView item with customizable text, icons, and background colors.
+
+### Customizable Swipe UI
+Control the icon, text, size, positioning, and background color for each swipe direction - designed to always stay behind the item as it moves.
+
+### ↔Intelligent Swipe Layout Handling
+Text and icons automatically adjust based on swipe distance to avoid overlap, snap, or clipping.
+
+### Drag-to-Reorder (Full-Row Drag)
+Enable item reordering by dragging anywhere on the item — no extra handles needed.
+
+### Drag Handle Support
+Prefer a drag handle? Let users reorder only when they hold a specific view (e.g., a “grip” icon) inside the item.
+
+### Plug-and-Play Integration
+Works with any RecyclerView + adapter setup with minimal boilerplate. No need to override multiple callbacks or write custom ItemTouchHelpers.
+
+### Smart Coordinate & Position Management
+Automatically calculates item movement, displacement (dX), icon/text placement, and swipe background area — so everything aligns perfectly.
+
+### Lightweight & Dependency-Free
+No unnecessary dependencies — the library is small, efficient, and built completely on Android's ItemTouchHelper.
+
+## Installation & Usage
+
+---
+#### Add the dependency in app's `build.gradle` file:
+
+```gradle
+dependencies {
+    implementation("io.github.pparekh2009:recyclerplus:1.0.0")
+}
+```
+
+#### Configure Swipe & Drag Behavior
+
+```kotlin
+val recyclerView = findViewById(R.id.recyclerview)
+
+val leftSwipeConfig = SwipeConfig.Builder()
+    .setText("Delete") 
+    .setBackgroundColor("#ba280b".toColorInt())
+    .setTextSize(50f)
+    .setTextColor(Color.WHITE)
+    .setIcon(this, R.drawable.rounded_delete_24)
+    .setIconTint(Color.WHITE)
+    .setIconSize(80)
+    .setOnSwipeListener { position ->
+        itemList.removeAt(position)
+        itemViewAdapter?.notifyItemRemoved(position)
+        Toast.makeText(this, "Swiped Left: $position", Toast.LENGTH_SHORT).show()
+    }
+    .build()
+
+val rightSwipeConfig = SwipeConfig.Builder()
+    .setText("Edit")
+    .setBackgroundColor("#1d3ee0".toColorInt())
+    .setTextSize(50f)
+    .setTextColor(Color.WHITE)
+    .setIcon(this, R.drawable.rounded_delete_24)
+    .setIconTint(Color.WHITE)
+    .setIconSize(80)
+    .setOnSwipeListener { position ->
+        Toast.makeText(this, "Swiped Right: $position", Toast.LENGTH_SHORT).show()
+    }
+    .build()
+
+// Configure RecyclerPlus
+val recyclerPlusTouchHelper = RecyclerPlusTouchHelper.Builder()
+    .enableDrag()         // Drag whole row
+    .enableDragOnView()   // Drag using a specific view (optional)
+    .setLeftSwipeConfig(leftSwipeConfig)
+    .setRightSwipeConfig(rightSwipeConfig)
+    .create()
+
+// Attach helper to RecyclerView
+binding.itemList.setTouchHelper(recyclerPlusTouchHelper)
+``` 
+
+#### Optional: Drag using specific view
+
+If you use `.enableDragOnView()`, pass recyclerPlusTouchHelper to recyclerView adapter
+```kotlin
+itemViewAdapter = ItemListAdapter(itemList, recyclerPlusTouchHelper)
+```
+And add this in the adapter's `onBindViewHolder()` function
+
+```kotlin
+dragView.setOnLongClickListener {
+    recyclerPlusTouchHelper.startDrag(holder)
+    true
+}
+```
+This allows dragging only when the user holds the specific “drag handle” view.
+
+## Screenshots
+
+---
+
+| Left Swipe                               | Right Swipe                                | Drag to Reorder                         |
+|------------------------------------------|--------------------------------------------|-----------------------------------------|
+| ![Left Swipe](images/left_swipe_gif.gif) | ![Right Swipe](images/right_swipe_gif.gif) | ![Drag to Reorder](images/drag_gif.gif) |
+
+
+## License
+
+---
+This project is licensed under the MIT License – see the [LICENSE](LICENSE) file for details.
--- recyclerplus/build.gradle.kts
+++ recyclerplus/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.android.build.gradle.LibraryExtension
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.library)
@@ -7,102 +8,128 @@ plugins {
     id("signing")
 }
 
+// load gradle.properties
+val localProps = Properties()
+val propsFile = rootProject.file("gradle.properties")
+if (propsFile.exists()) {
+    localProps.load(propsFile.inputStream())
+}
+
 android {
     namespace = "com.priyanshparekh.recyclerplus"
     compileSdk = 35
 
-//    afterEvaluate {
-//        // Task for sources jar
-//        val androidSourcesJar by tasks.register<Jar>("androidSourcesJar") {
-//            archiveClassifier.set("sources")
-//            val android = extensions.findByType<LibraryExtension>()
-//            from(android?.sourceSets?.getByName("main")?.java?.srcDirs)
-//        }
-//
-//        // Task for javadoc jar (placeholder)
-//        val androidJavadocsJar by tasks.register<Jar>("androidJavadocsJar") {
-//            archiveClassifier.set("javadoc")
-//            from("$projectDir/README.md")
-//        }
-//
-//        tasks.register("generateCheckSums") {
-//            group = "verification"
-//            description = "Generates MD5 and SHA1 checksums for all release artifacts"
-//
-//            doLast {
+    afterEvaluate {
+        // Task for sources jar
+        val androidSourcesJar by tasks.register<Jar>("androidSourcesJar") {
+            archiveClassifier.set("sources")
+            val android = extensions.findByType<LibraryExtension>()
+            from(android?.sourceSets?.getByName("main")?.java?.srcDirs)
+        }
+
+        // Task for javadoc jar (placeholder)
+        val androidJavadocsJar by tasks.register<Jar>("androidJavadocsJar") {
+            archiveClassifier.set("javadoc")
+            from("$projectDir/README.md")
+        }
+
+        tasks.register("generateCheckSums") {
+            group = "verification"
+            description = "Generates MD5 and SHA1 checksums for all release artifacts"
+
+            doLast {
+
+                val artifactDir = File(System.getProperty("user.home") +
+                        "/.m2/repository/io/github/pparekh2009/recyclerplus/1.0.0")
+
+                if (!artifactDir.exists()) {
+                    println("Artifact directory not found: $artifactDir")
+                    return@doLast
+                }
 //                val artifacts = listOf(
 //                    file("$buildDir/outputs/aar/${project.name}-release.aar"),
 //                    file("$buildDir/libs/${project.name}-release-sources.jar"),
 //                    file("$buildDir/libs/${project.name}-release-javadoc.jar"),
 //                    file("$buildDir/publishing/release/${project.name}-1.0.0.pom"),
 //                )
-//
-//                artifacts.forEach { artifact ->
-//                    if (artifact.exists()) {
-//                        ant.withGroovyBuilder {
-//                            "checksum"("file" to artifact, "algorithm" to "MD5", "todir" to artifact.parent)
-//                            "checksum"("file" to artifact, "algorithm" to "SHA1", "todir" to artifact.parent)
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        publishing {
-//            publications {
-//                create<MavenPublication>("release") {
-//                    groupId = "io.github.pparekh2009"
-//                    artifactId = "recyclerplus"
-//                    version = "1.0.0"
-//
-//                    // Publish the AAR file
-//                    artifact("$buildDir/outputs/aar/${project.name}-release.aar")
-//
-//                    // Attach sources + javadoc jars
-//                    artifact(androidSourcesJar)
-//                    artifact(androidJavadocsJar)
-//
-//                    pom {
-//                        name.set("RecyclerPlus")
-//                        description.set("A Recyclerview utility library for Android")
-//                        url.set("https://github.com/pparekh2009/RecyclerPlus")
-//                        licenses {
-//                            license {
-//                                name.set("The Apache License, Version 2.0")
-//                                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-//                            }
-//                        }
-//                        developers {
-//                            developer {
-//                                id.set("pparekh2009")
-//                                name.set("Priyansh Parekh")
-//                            }
-//                        }
-//                        scm {
-//                            connection.set("scm:git:github.com/pparekh2009/RecyclerPlus.git")
-//                            developerConnection.set("scm:git:ssh://github.com/pparekh2009/RecyclerPlus.git")
-//                            url.set("https://github.com/pparekh2009/RecyclerPlus")
-//                        }
-//                    }
-//                }
-//            }
-//
-//            repositories {
-//                maven {
-//                    name = "OSSRH"
-//                    url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-//                    credentials {
-//                        username = findProperty("ossrhUsername") as String?
-//                        password = findProperty("ossrhPassword") as String?
-//                    }
-//                }
-//            }
-//        }
-//
-////        signing {
-////            sign(publishing.publications["release"])
-////        }
-//    }
+
+                artifactDir.listFiles { f -> f.isFile && !f.name.endsWith(".md5") && !f.name.endsWith(".sha1") && !f.name.endsWith(".asc")}
+                    ?.forEach { artifact ->
+                        if (artifact.exists()) {
+                            ant.withGroovyBuilder {
+                                "checksum"("file" to artifact, "algorithm" to "MD5", "todir" to artifact.parent)
+                                "checksum"("file" to artifact, "algorithm" to "SHA1", "todir" to artifact.parent)
+                            }
+                        }
+                    }
+            }
+        }
+
+        publishing {
+            publications {
+                create<MavenPublication>("release") {
+                    groupId = project.findProperty("GROUP") as String
+                    artifactId = project.findProperty("POM_ARTIFACT_ID") as String
+                    version = project.findProperty("VERSION_NAME") as String
+
+                    // Publish the AAR file
+                    artifact("$buildDir/outputs/aar/${project.name}-release.aar")
+
+                    // Attach sources + javadoc jars
+                    artifact(androidSourcesJar)
+                    artifact(androidJavadocsJar)
+
+                    pom {
+                        name.set("RecyclerPlus")
+                        description.set("A Recyclerview utility library for Android")
+                        url.set("https://github.com/pparekh2009/RecyclerPlus")
+                        licenses {
+                            license {
+                                name.set("MIT License")
+                                url.set("https://opensource.org/licenses/MIT")
+                            }
+                        }
+                        developers {
+                            developer {
+                                id.set("pparekh2009")
+                                name.set("Priyansh Parekh")
+                            }
+                        }
+                        scm {
+                            connection.set("scm:git:github.com/pparekh2009/RecyclerPlus.git")
+                            developerConnection.set("scm:git:ssh://github.com/pparekh2009/RecyclerPlus.git")
+                            url.set("https://github.com/pparekh2009/RecyclerPlus")
+                        }
+                    }
+                }
+            }
+
+            repositories {
+                maven {
+                    name = "sonatype"
+                    url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    credentials {
+                        username = project.findProperty("SONATYPE_USERNAME") as String?
+                        password = project.findProperty("SONATYPE_PASSWORD") as String?
+                    }
+                }
+            }
+        }
+
+        tasks.matching { it.name == "signReleasePublication" }.configureEach {
+            dependsOn(tasks.named("bundleReleaseAar"))
+        }
+
+        tasks.matching { it.name == "publishToMavenLocal" }.configureEach {
+            finalizedBy(tasks.named("generateCheckSums"))
+        }
+
+
+
+        signing {
+            sign(publishing.publications["release"])
+        }
+    }
 
     defaultConfig {
         minSdk = 24
